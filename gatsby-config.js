9354575b16dccd0524436d47eda44834df043f88
leerltx <leerltx@gmail.com>
2019-07-30 14:54:11 +0800

    new

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Barry Willson',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>It's Willson Barry' Place</h1>
+    <p>I like every all around the world</p>
+    <p>How about you?</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
